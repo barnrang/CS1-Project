289233f1e2c47e56dee1bb817827890eea7c4383
NAPAT <barnrang@gmail.com>
2018-10-30 18:23:00 +0900

    add hw

--- project2/dec.py
+++ project2/dec.py
@@ -1,22 +1,66 @@
+'''
+T
+
+
+'''
 from time import sleep
+import sys
+from decimal import *
+
 d = int(input("Input a denominator: "))
 count = 0
 done = False
 x = 1
-used = [0 for _ in range(d+1)]
-used[1] = 1
+used = [-1 for _ in range(d+1)]
+loop_st_idx = None
+used[1] = 0
 
 while not done:
     count += 1
     x *= 10
     q = x // d
     r = x % d
-    print(f'{count}:{q}({r})')
-    sleep(0.5)
     if r == 0:
         done = True
     else:
-        if used[r] == 0:
-            used[r] = 1
+        x = r
+        if used[r] == -1:
+            used[r] = count
         else:
             done = True
+            loop_st_idx = used[r]
+
+# Print Function
+done = False
+used = [-1 for _ in range(d+1)]
+count = 0
+x = 1
+used[1] = 0
+
+print(f'1/{d} = 0.', end='', flush=True)
+
+while not done:
+    if loop_st_idx is not None and count == loop_st_idx:
+        print('(', end='', flush=True)
+    count += 1
+    x *= 10
+    q = x // d
+    r = x % d
+    print(f'{q}', end='', flush=True)
+    sleep(0.02)
+    if r == 0:
+        done = True
+    else:
+        x = r
+        if used[r] == -1:
+            used[r] = count
+        else:
+            done = True
+
+if loop_st_idx is not None:
+    print(')')
+else:
+    print('')
+
+getcontext().prec = 50
+print(f'Actual value = {(Decimal(1)/Decimal(d)).to_eng_string()}')
